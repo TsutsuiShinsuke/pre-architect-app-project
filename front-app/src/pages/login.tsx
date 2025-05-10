d02fed092f3648e8cdd831085a6643c13baacac7
TsutsuiShinsuke <tsutsuics21@gmail.com>
2025-05-10 14:27:02 +0900

    Gitテストメッセージ(0510)

--- front-app/src/pages/login.tsx
+++ front-app/src/pages/login.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import React from "react";
+import Link from 'next/link';
 
 function Login () {
   return (
     <div>
-      login
+
+      <h2>
+      login0510  
+      </h2>
+      
+      <Link href="/signup">
+       signupページ
+      </Link>
+
     </div>
   );
 
